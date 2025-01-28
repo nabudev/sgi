5eea3a6723e313f113c7ab467cc3cc11b0880f46
nahueldev <nabudev01@gmail.com>
2025-01-28 16:56:55 -0300

    creacion de la seccion mis inscripciones

--- src/app/inscripciones/page.tsx
+++ src/app/inscripciones/page.tsx
@@ -1,1 +1,15 @@
+import InforInscripciones from "@/components/InfoInscripciones";
+import Navbar from "@/components/Navbar";
 
+
+export default function Inscripciones(){
+    return (
+        <>
+        <Navbar/>
+        <br>
+        </br>
+        <br />
+        <InforInscripciones/>
+        </>
+    )
+}
